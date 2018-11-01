30c6edd1efedbb3152f18ea60736ad7bfe3dc357
Timmy <timgabrielnguyen@gmail.com>
2018-11-01 13:55:44 -0700

    Instantiate paddles

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,5 +1,6 @@
-import { SVG_NS } from "../settings";
 import Board from "./Board";
+import Paddle from "./Paddle";
+import { SVG_NS, KEYS } from "../settings";
 
 export default class Game {
   constructor(element, width, height) {
@@ -9,7 +10,33 @@ export default class Game {
 
     this.gameElement = document.getElementById(this.element);
     this.board = new Board(this.width, this.height);
-  }
+
+    this.paddleWidth = 8;
+    this.paddleHeight = 56;
+    this.boardGap = 10;
+
+    this.player1 = new Paddle(
+      this.height,
+      this.paddleWidth,
+      this.paddleHeight,
+      this.boardGap,
+      (this.height - this.paddleHeight) / 2,
+      KEYS.a,
+      KEYS.z
+    );
+
+    this.player2 = new Paddle(
+      this.height,
+      this.paddleWidth,
+      this.paddleHeight,
+      this.width- this.boardGap - this.paddleWidth,
+      (this.height - this.paddleHeight) / 2,
+      KEYS.a,
+      KEYS.z
+    );
+
+    console.log(this.player1);
+  } //end of constructor
 
   render() {
     // More code goes here...
@@ -21,5 +48,7 @@ export default class Game {
     svg.setAttributeNS(null, "viewBox", `0 0 ${this.width} ${this.height}`);
     this.gameElement.appendChild(svg);
     this.board.render(svg);
+    this.player1.render(svg);
+    this.player2.render(svg);
   }
 }
