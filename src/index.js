bf619c28d343f8882f0a935d477bd73e44920185
Timmy <timgabrielnguyen@gmail.com>
2018-11-01 10:48:36 -0700

    Add Append SVG

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import './styles/game.css';
-import Game from './partials/Game'
+import "./styles/game.css";
+import Game from "./partials/Game";
 
 // create a game instance
-const game = new Game('game', 512, 256);
+const game = new Game("game", 512, 256);
 
 (function gameLoop() {
-    game.render();
-    requestAnimationFrame(gameLoop);
+
+  game.render();
+  requestAnimationFrame(gameLoop);
 })();
