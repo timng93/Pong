7eae7ea8247f2b2312741767383dea58f514f074
Timmy <timgabrielnguyen@gmail.com>
2018-11-02 10:26:51 -0700

    Ball starts to bounce off the wall

--- src/partials/Ball.js
+++ src/partials/Ball.js
@@ -7,30 +7,41 @@ export default class Ball {
     this.boardHeight = boardHeight;
     this.direction = 1;
 
-
-   
-
     this.reset();
   }
 
   reset() {
     this.x = this.boardWidth / 2;
     this.y = this.boardHeight / 2;
-    
+
     this.vy = 0;
-    while( this.vy === 0) {
-      this.vy = Math.floor(Math.random() * 10 -5);
+    while (this.vy === 0) {
+      this.vy = Math.floor(Math.random() * 10 - 5);
     }
-      this.vx = this.direction * (6 - Math.abs(this.vy));
+    this.vx = this.direction * (6 - Math.abs(this.vy));
   }
 
-  
+  wallCollision() {
+    const hitLeft = this.x - this.radius <= 0;
+    const hitRight = this.x + this.radius >= this.boardWidth;
+    const hitTop = this.y - this.radius <= 0;
+    const hitBottom = this.y + this.radius >= this.boardHeight;
+
+    if (hitLeft || hitRight) {
+      this.vx *= -1;
+    } else if (hitTop) {
+      this.vy *= -1;
+    } else if (hitBottom) {
+      this.vy *= -1;
+    }
+  }
 
   render(svg) {
-
     this.x += this.vx;
     this.y += this.vy;
-     
+
+    this.wallCollision();
+
     let circle = document.createElementNS(SVG_NS, "circle");
     circle.setAttributeNS(null, "fill", "gold");
     circle.setAttributeNS(null, "cx", this.x);
